51a1eac450db28a607b44fd61739a1e6454cb684
guk1987 <guk1987@naver.com>
2024-02-15 01:29:17 +0900

    다시 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
- return <div>
-    <h1> Hello World </h1>
-  </div> ;
+  return (
+    <div> hello world
+    </div>
+  );
 }
 
 export default App;
